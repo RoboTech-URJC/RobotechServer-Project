eae3a94e0e2da8a87874330c42071c4b9cae6cf5
Noozh <sirl.daniel.arevalo@hotmail.com>
2018-11-19 18:47:28 +0100

    Logo changes

--- html/php/register.php
+++ html/php/register.php
@@ -6,17 +6,4 @@
     $degree = strip_tags($_POST['degree']);
     $password = strip_tags($_POST['password']);
 
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "robotechurjc@gmail.com";
-    $to = "pirosflame@gmail.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    $bool = mail($to,$subject,$message, $headers);
-    if($bool){
-        echo "Mensaje enviado";
-    }else{
-        echo "Mensaje no enviado";
-    }
 ?>
