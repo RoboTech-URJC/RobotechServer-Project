4d4186f2a57367719c188716c0cc543c0f5a364f
Fernando <fergonzaramos@yahoo.es>
2018-11-22 20:06:26 +0100

    [@media style.css]Adjustment for mobile

--- html/index.php
+++ html/index.php
@@ -531,62 +531,52 @@
         <!-- @component: footer -->
 
         <section id="contact">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h2 class="section-title">Contacta con Nosotros</h2>
-                    </div>
-                </div>
+          <h2 text-align="center">Contacta con Nosotros</h2>
+              <div class="info">
 
-                <div class="row justify-content-center">
-                    <div class="col-lg-3 col-md-4">
-                        <div class="info">
-                            <div>
-                                <i class="fa fa-map-marker"></i>
-                                <p>Camino del Molino, s/n, 28942 Fuenlabrada<br>(Madrid)</p>
-                            </div>
+                  <div>
+                      <i class="fa fa-envelope"></i>
+                          <p>info@example.com</p>
+                  </div>
 
-                            <div>
-                                <i class="fa fa-envelope"></i>
-                                <p>info@example.com</p>
-                            </div>
+                  <div>
+                      <i class="fa fa-phone"></i>
+                          <p>+1 5589 55488 55s</p>
+                  </div>
+                  <i class="fa fa-map-marker"></i>
+                      <p>Camino del Molino, s/n, 28942 Fuenlabrada<br>(Madrid)</p><br><br>
 
-                            <div>
-                                <i class="fa fa-phone"></i>
-                                <p>+1 5589 55488 55s</p>
-                            </div>
+                      <div class="contact-map">
+                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3043.6827673403045!2d-3.8212035799414474!3d40.
+                          2827966514908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd418b7f310d2417%3A0x8bb94231c370b55!2sUniversidad+Rey+
+                          Juan+Carlos%3A+Campus+de+Fuenlabrada!5e0!3m2!1ses!2ses!4v1542847758229" width="600" height="450" frameborder="0"
+                          style="border:0" allowfullscreen></iframe>
+                      </div>
+              </div>
 
-                        </div>
-                    </div>
-
-                    <div class="col-lg-5 col-md-8">
-                        <div class="form">
-                            <div id="sendmessage">Su mensaje ha sido enviado. Gracias!</div>
-                            <div id="errormessage"></div>
-                            <form action="" method="post" role="form" class="contactForm">
-                                <div class="form-group">
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="form-group">
-                                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensaje"></textarea>
-                                    <div class="validation"></div>
-                                </div>
-                                <div class="text-center"><button type="submit">Enviar Mensaje</button></div>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+              <div class="form">
+                  <div id="sendmessage">Su mensaje ha sido enviado. Gracias!</div>
+                  <div id="errormessage"></div>
+                  <form action="" method="post" role="form" class="contactForm">
+                      <div class="form-group">
+                          <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                          <div class="validation"></div>
+                      </div>
+                      <div class="form-group">
+                          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                          <div class="validation"></div>
+                      </div>
+                      <div class="form-group">
+                          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                          <div class="validation"></div>
+                      </div>
+                      <div class="form-group">
+                          <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensaje"></textarea>
+                          <div class="validation"></div>
+                      </div>
+                      <div class="text-center"><button type="submit">Enviar Mensaje</button></div>
+                  </form>
+              </div>
         </section>
 
         <?php
