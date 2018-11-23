001a0f7b5cb3a32161ea4cfc69746e510ec860ed
Noozh <sirl.daniel.arevalo@hotmail.com>
2018-11-23 22:44:12 +0100

    [index.php,style.css]Responsive update

--- html/index.php
+++ html/index.php
@@ -62,7 +62,7 @@
                 <br><br><br>
                 <div class="col-md-12">
                     <h1 class="">
-                    Asociación de Tecnología de la URJC
+                    Asociación de Tecnología de la <b><span style="color:red">URJC</span></b>
                     </h1>
                 </div>
             </div>
@@ -93,7 +93,7 @@
                 <source src="videos/video_presentation.mp4" type="video/mp4"/>
             </video/>
             <div class="video-text">
-                <div class = "text-font">
+                <div class = "text-font" style="margin-top:15%;margin-right:20%">
                     Para aprobar con Marqués, Únete a Robotech!
                 </div>
             </div>
@@ -530,53 +530,50 @@
         <!-- /Team -->
         <!-- @component: footer -->
 
-        <section id="contact">
-          <h2 text-align="center">Contacta con Nosotros</h2>
-              <div class="info">
+        <section id="contact" >
+            <h2 text-align="center">Contacta con Nosotros</h2>
+            <div class="info row" style="max-width:90%;margin-left:5%">
 
-                  <div>
-                      <i class="fa fa-envelope"></i>
-                          <p>info@example.com</p>
-                  </div>
+                <div class="col-md-4">
+                    <i class="fa fa-envelope"></i>
+                    <p>info@example.com</p>
+                </div>
 
-                  <div>
-                      <i class="fa fa-phone"></i>
-                          <p>+1 5589 55488 55s</p>
-                  </div>
-                  <i class="fa fa-map-marker"></i>
-                      <p>Camino del Molino, s/n, 28942 Fuenlabrada<br>(Madrid)</p><br><br>
+                <div class="col-md-4">
+                    <i class="fa fa-phone"></i>
+                    <p>+1 5589 55488 55s</p>
+                </div>
 
-                      <div class="contact-map">
-                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3043.6827673403045!2d-3.8212035799414474!3d40.
-                          2827966514908!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd418b7f310d2417%3A0x8bb94231c370b55!2sUniversidad+Rey+
-                          Juan+Carlos%3A+Campus+de+Fuenlabrada!5e0!3m2!1ses!2ses!4v1542847758229" width="600" height="450" frameborder="0"
-                          style="border:0" allowfullscreen></iframe>
-                      </div>
-              </div>
-
-              <div class="form">
-                  <div id="sendmessage">Su mensaje ha sido enviado. Gracias!</div>
-                  <div id="errormessage"></div>
-                  <form action="" method="post" role="form" class="contactForm">
-                      <div class="form-group">
-                          <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                          <div class="validation"></div>
-                      </div>
-                      <div class="form-group">
-                          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                          <div class="validation"></div>
-                      </div>
-                      <div class="form-group">
-                          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                          <div class="validation"></div>
-                      </div>
-                      <div class="form-group">
-                          <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensaje"></textarea>
-                          <div class="validation"></div>
-                      </div>
-                      <div class="text-center"><button type="submit">Enviar Mensaje</button></div>
-                  </form>
-              </div>
+                <div class="col-md-4">
+                    <i class="fa fa-map-marker"></i>
+                    <p>Camino del Molino, s/n, 28942 Fuenlabrada <br>(Madrid)</p>
+                </div>
+                <div class="col-md-12">
+                    <div class="form" >
+                        <div id="sendmessage">Su mensaje ha sido enviado. Gracias!</div>
+                        <div id="errormessage"></div>
+                        <form action="" method="post" role="form" class="contactForm">
+                            <div class="form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                                <div class="validation"></div>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Mensaje"></textarea>
+                                <div class="validation"></div>
+                            </div>
+                            <div class="text-center"><button type="submit">Enviar Mensaje</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <?php
