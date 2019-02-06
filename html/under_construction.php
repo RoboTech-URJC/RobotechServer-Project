e15ab229fddbd95bb53fc2012b71552219a8e5e5
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-02-06 11:25:12 +0100

    under construction page has been created

---
+++ html/under_construction.php
@@ -0,0 +1,57 @@
+<head>
+    <meta charset="utf-8">
+    <title>RoboTech URJC</title>
+    <link href="/img/ico.ico" rel="shortcut icon" type="image/x-icon" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Facebook Opengraph integration: https://developers.facebook.com/docs/sharing/opengraph -->
+    <meta property="og:title" content="">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="">
+
+    <!-- Twitter Cards integration: https://dev.twitter.com/cards/  -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,700|Roboto:400,900" rel="stylesheet">
+
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Libraries CSS Files -->
+    <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Main Stylesheet File -->
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<section class="undercons">
+    <div class="container text-center">
+        <div class="row">
+            <table border="0" width="auto" height="auto"><tr>
+                <td style="width:25%"><center></center></td>
+                <td><center><img  class="logo-responsive" src="img/logo_no_underscore.png" border="0"></center></td>
+                <td><center><img  class="logo-responsive" name="underscore" src="img/underscore.png" border="0"></center></td>
+                <td style="width:25%"><center></center></td>
+            </tr></table>
+        </div>
+        <br><br><br>
+        <div class="col-md-12">
+            <h1 class="">
+            Sitio web en <b><span style="color:red">construccion</span></b>
+            </h1>
+        </div>
+    </div>
+</section>
