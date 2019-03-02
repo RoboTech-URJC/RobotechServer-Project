f07768b1722513878c2587b4fc66a0fee3ba9757
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-03-02 11:58:14 +0100

    [report.php] Query + pdfgen now are working

--- html/php/report.php
+++ html/php/report.php
@@ -3,30 +3,35 @@
     include 'plantilla.php';
 
     $link = bbdd_connection("localhost","root","asddsa","members");
+	$query = "SELECT * FROM members";
+	
+	$rs =mysqli_query($link,$query);
 
-    // echo $link;
+	$query = mysqli_fetch_row($rs);
+	$value_output = $query[1];
 
-	$query = "SELECT SURNAME FROM members";
+	genpdf($value_output);
 
-	$resultado = mysqli_query($link, $query);
+	function genpdf($val){
+		$pdf = new PDF();
+		$pdf->AliasNbPages();
+		$pdf->AddPage();
 
-	$pdf = new PDF();
-	$pdf->AliasNbPages();
-	$pdf->AddPage();
+		$pdf->SetFillColor(232,232,232);
+		$pdf->SetFont('Arial','B',12);
+		$pdf->Cell(70,6,'Nombre',1,0,'C',1);
+		// $pdf->Cell(20,6,'Apellido',1,0,'C',1);
+		// $pdf->Cell(70,6,'Grado',1,1,'C',1);
 
+		$pdf->SetFont('Arial','',10);
 
-	$pdf->SetFillColor(232,232,232);
-	$pdf->SetFont('Arial','B',12);
-	$pdf->Cell(70,6,'Nombre',1,0,'C',1);
-	// $pdf->Cell(20,6,'Apellido',1,0,'C',1);
-	// $pdf->Cell(70,6,'Grado',1,1,'C',1);
+		$pdf->Cell(70,6,$val,1,0,'C');
 
-	$pdf->SetFont('Arial','',10);
-    echo $resultado['SURNAME'];while($row = $resultado)
-	{
-		$pdf->Cell(70,6,utf8_decode($row['SURNAME']),1,0,'C');
-		// $pdf->Cell(20,6,$row['SURNAME'],1,0,'C');
-		// $pdf->Cell(70,6,utf8_decode($row['GRADO']),1,1,'C');
+		// while($row = mysqli_fetch_array($rs)){
+		// 	$pdf->Cell(70,6,utf8_decode('pepe'),1,0,'C');
+		// 	// $pdf->Cell(20,6,$row['SURNAME'],1,0,'C');
+		// 	// $pdf->Cell(70,6,utf8_decode($row['GRADO']),1,1,'C');
+		// }
+		$pdf->Output();
 	}
-	$pdf->Output();
 ?>
