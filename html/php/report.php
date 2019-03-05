fa4ffb38ea6bf96fd9c5396f2c03489f6cba5b5d
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-03-05 23:18:18 +0100

    [report.php] Generating member list bases on bbdd is possible now

--- html/php/report.php
+++ html/php/report.php
@@ -1,37 +1,38 @@
 <?php
 	include 'lib/bbdd_easy_interface.php';
     include 'plantilla.php';
-
     $link = bbdd_connection("localhost","root","asddsa","members");
 	$query = "SELECT * FROM members";
-	
+
 	$rs =mysqli_query($link,$query);
 
-	$query = mysqli_fetch_row($rs);
-	$value_output = $query[1];
+	$rows = mysqli_num_rows($rs);
+	membersinfo($rows,$rs);
 
-	genpdf($value_output);
-
-	function genpdf($val){
+	function membersinfo($rows,$rs){
 		$pdf = new PDF();
 		$pdf->AliasNbPages();
 		$pdf->AddPage();
-
+		$pdf->Header('Listado de miembros a fecha de: '.date('l jS \of F Y'));
 		$pdf->SetFillColor(232,232,232);
 		$pdf->SetFont('Arial','B',12);
-		$pdf->Cell(70,6,'Nombre',1,0,'C',1);
-		// $pdf->Cell(20,6,'Apellido',1,0,'C',1);
-		// $pdf->Cell(70,6,'Grado',1,1,'C',1);
-
+		$pdf->Cell(30,6,'Nombre',1,0,'C',1);
+		$pdf->Cell(30,6,'Apellido',1,0,'C',1);
+		$pdf->Cell(30,6,'DNI',1,0,'C',1);
+		$pdf->Cell(70,6,'URJC mail',1,0,'C',1);
+		$pdf->Cell(30,6,'Grado',1,0,'C',1);
+		$pdf->Ln();
 		$pdf->SetFont('Arial','',10);
+		for ($i = 0; $i < $rows; $i++) {
+			$arr = mysqli_fetch_row($rs);
+			$pdf->Cell(30,6,$arr[1],1,0,'C',1);
+			$pdf->Cell(30,6,$arr[2],1,0,'C',1);
+			$pdf->Cell(30,6,$arr[3],1,0,'C',1);
+			$pdf->Cell(70,6,$arr[4],1,0,'C',1);
+			$pdf->Cell(30,6,$arr[5],1,0,'C',1);
+			$pdf->Ln();
+		}
+		$pdf->Output('',$name='memberlist.pdf');
 
-		$pdf->Cell(70,6,$val,1,0,'C');
-
-		// while($row = mysqli_fetch_array($rs)){
-		// 	$pdf->Cell(70,6,utf8_decode('pepe'),1,0,'C');
-		// 	// $pdf->Cell(20,6,$row['SURNAME'],1,0,'C');
-		// 	// $pdf->Cell(70,6,utf8_decode($row['GRADO']),1,1,'C');
-		// }
-		$pdf->Output();
 	}
 ?>
