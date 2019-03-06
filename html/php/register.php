ddb746b8f515b1f9a1205bf1651bedf1f769f337
Noozh <sirl.daniel.arevalo@hotmail.com>
2019-03-06 09:06:11 +0100

    [register.php] Tab fixed

--- html/php/register.php
+++ html/php/register.php
@@ -79,12 +79,12 @@
         $phpmailer->Send();
     }
     function generateRandomString($length = 20) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 ?>
